db650a03527c07e9939a0074a96883328564275b
yushuai <yushuailover@163.com>
2018-06-15 17:27:16 +0800

    修改天顶角由80到180

--- SNO/dm_sno_cross_calc_core_snox.py
+++ SNO/dm_sno_cross_calc_core_snox.py
@@ -8,11 +8,14 @@ import os
 import sys
 import warnings
 
-from PB.pb_space import distance_GreatCircle, distance_GreatCircle_np, solar_zen
+from PB.pb.sat import solar_zen
+from PB.pb_space import distance_GreatCircle, distance_GreatCircle_np
 from PB.pb_time import ymd_plus, get_local_time
 from dm_sno_cross_calc_map import draw_fixed, draw_polar, draw_world, draw_china
 from shapely.geometry import LineString
 import numpy as np
+
+
 reload(sys)
 sys.setdefaultencoding('utf8')
 
